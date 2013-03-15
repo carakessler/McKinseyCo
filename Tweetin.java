832035af72a13478b20cfc40f1840fdbc5865e36
carakessler <ckessle2@u.rochester.edu>
2013-03-14 21:32:14 -0300

    Update Tweetin.java

--- Tweetin.java
+++ Tweetin.java
@@ -18,12 +18,12 @@ public class Tweetin {
 			while ((input = in.readLine()) != null) { //while there is input in bufferedreader 
 				Scanner s = new Scanner(input).useDelimiter(",");
 				//System.out.println(input);
-				String match ="display_url"; //the portion of each input we actually care about
+				String finder ="display_url"; //the portion of each input we actually care about
 				while(s.hasNext()) {
 					tweet2=s.next(); 
 					//System.out.println(tweet2);
 					if(tweet2.length()>=12) { //if the beginning of the token is long enough to be what we are looking for, display_url. without this if statement i was getting array out of bounds errors		            	 
-						if(tweet2.substring(1,12).equals(match)) { //if tweet2 without the quotes it has in the input is a display_url
+						if(tweet2.substring(1,12).equals(finder)) { //if tweet2 without the quotes it has in the input is a display_url
 							tweet = tweet2.substring(15, (tweet2.length()-1)); //the actual URL begins at spot 15 in the token, and ends with a quote, which is why i ended the substring one before the length of the substring
 							tweet=tweet.replaceAll("/",""); //remove random /s 
 							System.out.println(tweet);							
